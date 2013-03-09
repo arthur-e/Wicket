c0b50392f22d079ce87be222b8acb7663cb87b09
Arthur Endsley <kaendsle@mtu.edu>
2013-03-09 18:16:44 -0500

    Working POINT and MULTIPOINT construction in ArcGIS extension

--- wicket-arcgis.src.js
+++ wicket-arcgis.src.js
@@ -1,4 +1,30 @@
-/*global L, Wkt, console*/
+/*global dojo, esri, Wkt, console*/
+/** @license
+ *
+ *  Copyright (C) 2012 K. Arthur Endsley (kaendsle@mtu.edu)
+ *  Michigan Tech Research Institute (MTRI)
+ *  3600 Green Court, Suite 100, Ann Arbor, MI, 48105
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* @preserve
+ * NOTE: The ESRI ArcGIS API extension requirest JavaScript 1.6 or higher, due
+    its dependence on the Array functions map, indexOf, and lastIndexOf
+ */
+
 Wkt.Wkt.prototype.isRectangle = false;
 
 /**
@@ -24,6 +50,27 @@ Wkt.Wkt.prototype.construct = {
         }
 
         return new esri.geometry.Point(coord);
+    },
+
+    /**
+     * Creates the framework's equivalent multipoint geometry object.
+     * @param   config  {Object}    An optional properties hash the object should use
+     * @return          {esri.geometry.Multipoint}
+     */
+    multipoint: function (config) {
+        config = config || {};
+        config.spatialReference = (config.spatialReference || config.srs) ? config.spatialReference || config.srs : undefined;
+
+        return new esri.geometry.Multipoint({
+            // Create an Array of [x, y] coords from each point among the components
+            points: this.components.map(function (i) {
+                if (Wkt.isArray(i)) {
+                    i = i[0]; // Unwrap coords
+                }
+                return [i.x, i.y];
+            }),
+            spatialReference: config.spatialReference
+        });
     }
 };
 
