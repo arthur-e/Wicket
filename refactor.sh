a82f94b20eaa82fa9397add6eaf422f6336de66f
K. Arthur Endsley <kaendsle@mtu.edu>
2014-07-19 12:17:36 -0400

    Modified dependencies to try to fix Travis CI builds on older Node systems and removed extraneous refactor.sh

--- refactor.sh
+++
@@ -1,4 +0,0 @@
-node_modules/uglify-js/bin/uglifyjs wicket.js -o wicket.min.js
-node_modules/uglify-js/bin/uglifyjs wicket-leaflet.js -o wicket-leaflet.min.js
-node_modules/uglify-js/bin/uglifyjs wicket-arcgis.js -o wicket-arcgis.min.js
-node_modules/uglify-js/bin/uglifyjs wicket-gmap3.js -o wicket-gmap3.min.js
