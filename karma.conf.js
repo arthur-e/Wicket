ac4679ea1afa6a1e12fb1eaeb2fa356d5e761bb7
Felipe Figueroa <amenadiel@gmail.com>
2018-02-26 11:58:41 -0300

    waits for google to load, to pass karma tests
    flags to allow phantom to ignore ssl errors
    
    checks if window.google is an object
    
    checks if window.google is an object

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,28 @@
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    port: 9877,
-    colors: true,
-    logLevel: 'INFO',
-    autoWatch: false,
-    browsers: ['PhantomJS'],
-    singleRun: true,
-    frameworks: ['jasmine'],
-    reporters: ['mocha'],
-    files: [
-      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry',
-      'wicket.js',
-      'wicket-gmap3.js',
-      'tests/wicket-gmap3-spec.js'
-    ]
-  });
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        port: 9877,
+        colors: true,
+        logLevel: 'INFO',
+        autoWatch: false,
+        browsers: ['PhantomJS'],
+        phantomjsLauncher: {
+            // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+            exitOnResourceError: false,
+            flags: [
+                '--web-security=false',
+                '--load-images=false',
+                '--ignore-ssl-errors=true'
+            ]
+        },
+        singleRun: true,
+        frameworks: ['jasmine'],
+        reporters: ['mocha'],
+        files: [
+            'https://maps.googleapis.com/maps/api/js?v=3.31&libraries=geometry',
+            'wicket.js',
+            'wicket-gmap3.js',
+            'tests/wicket-gmap3-spec.js'
+        ]
+    });
 };
